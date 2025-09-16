a2fe252ae00b2dd8eeb404d6f8a37ad4f9c157f5
yisus82 <djalma8@gmail.com>
2025-09-16 13:53:58 +0200

    Añadir iconos a las pestañas

--- StickerSmash/app/(tabs)/_layout.tsx
+++ StickerSmash/app/(tabs)/_layout.tsx
@@ -1,9 +1,34 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { Tabs } from 'expo-router';
 
 const TabsLayout = () => (
-  <Tabs>
-    <Tabs.Screen name='index' options={{ title: 'Home' }} />
-    <Tabs.Screen name='about' options={{ title: 'About' }} />
+  <Tabs
+    screenOptions={{
+      tabBarActiveTintColor: '#ffd33d',
+    }}
+  >
+    <Tabs.Screen
+      name='index'
+      options={{
+        title: 'Home',
+        tabBarIcon: ({ color, focused }) => (
+          <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+        ),
+      }}
+    />
+    <Tabs.Screen
+      name='about'
+      options={{
+        title: 'About',
+        tabBarIcon: ({ color, focused }) => (
+          <Ionicons
+            name={focused ? 'information-circle' : 'information-circle-outline'}
+            color={color}
+            size={24}
+          />
+        ),
+      }}
+    />
   </Tabs>
 );
 
