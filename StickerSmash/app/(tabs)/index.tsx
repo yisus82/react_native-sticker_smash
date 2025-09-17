d0758a36108fbc11e880a303418cd809c037902b
yisus82 <djalma8@gmail.com>
2025-09-17 11:51:00 +0200

    Añadir imagen de fondo en Home

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,12 +1,13 @@
-import { Link } from 'expo-router';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'expo-image';
+import { StyleSheet, View } from 'react-native';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 const Index = () => (
   <View style={styles.container}>
-    <Text style={styles.text}>Home screen</Text>
-    <Link href='/about' style={styles.button}>
-      Go to About screen
-    </Link>
+    <View style={styles.imageContainer}>
+      <Image source={PlaceholderImage} style={styles.image} />
+    </View>
   </View>
 );
 
@@ -15,15 +16,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  text: {
-    color: '#fff',
+  imageContainer: {
+    flex: 1,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });
 
