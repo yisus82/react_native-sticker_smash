e8e38a0b81ce93a55df0f81c34f1b216eed50643
yisus82 <djalma8@gmail.com>
2025-09-18 11:51:55 +0200

    Añadir ImagePicker

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import * as ImagePicker from 'expo-image-picker';
 import { StyleSheet, View } from 'react-native';
 
 import Button from '@/components/Button';
@@ -5,17 +6,33 @@ import ImageViewer from '@/components/ImageViewer';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
-const Index = () => (
-  <View style={styles.container}>
-    <View style={styles.imageContainer}>
-      <ImageViewer imgSource={PlaceholderImage} />
+const Index = () => {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme='primary' label='Choose a photo' onPress={pickImageAsync} />
+        <Button label='Use this photo' />
+      </View>
     </View>
-    <View style={styles.footerContainer}>
-      <Button theme='primary' label='Choose a photo' />
-      <Button label='Use this photo' />
-    </View>
-  </View>
-);
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
--- StickerSmash/components/Button.tsx
+++ StickerSmash/components/Button.tsx
@@ -4,9 +4,10 @@ import { Pressable, StyleSheet, Text, View } from 'react-native';
 type Props = {
   label: string;
   theme?: 'primary';
+  onPress?: () => void;
 };
 
-const Button = ({ label, theme }: Props) => {
+const Button = ({ label, theme, onPress }: Props) => {
   if (theme === 'primary') {
     return (
       <View
@@ -15,10 +16,7 @@ const Button = ({ label, theme }: Props) => {
           { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
         ]}
       >
-        <Pressable
-          style={[styles.button, { backgroundColor: '#fff' }]}
-          onPress={() => alert('You pressed a button.')}
-        >
+        <Pressable style={[styles.button, { backgroundColor: '#fff' }]} onPress={onPress}>
           <FontAwesome name='picture-o' size={18} color='#25292e' style={styles.buttonIcon} />
           <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
         </Pressable>
